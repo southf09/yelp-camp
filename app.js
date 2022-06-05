093e288206c838fb20b0a1a068fb39b22b732cab
Frank South <frankstonesouth@gmail.com>
2022-06-05 16:32:45 -0400

    Began linking database via mongoose

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true, 
+   useUnifiedTopology: true 
+}, err => {
+   if(err) throw err;
+   console.log('Connected to MongoDB!!!')
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))

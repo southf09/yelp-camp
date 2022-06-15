ab4e46d466837a974e2fbcf8de911d0c1d46f4d7
Frank South <frankstonesouth@gmail.com>
2022-06-15 15:44:51 -0400

    Tested Mongo/Mongoose connection with a sample campground

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const Campground = require('./models/campground')
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true, 
-   useUnifiedTopology: true 
-}, err => {
-   if(err) throw err;
-   console.log('Connected to MongoDB!!!')
-});
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection Error"));
@@ -24,6 +19,11 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) =>{
     res.render('home')
+})
+app.get('/makecampground', async (req, res) =>{
+    const camp = new Campground({title: 'My Backyard', description: 'cheap camping!'});
+    await camp.save();
+    res.send(camp);
 })
 
 app.listen(3000, () => {

dc2422a39d7a394a95d523c5e7b53fe1a4e4c192
Frank South <frankstonesouth@gmail.com>
2022-06-18 11:00:08 -0400

    Added a cities database and seed function to generate sample db in mongo

---
+++ seeds/index.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const Campground = require('../models/campground')
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const seedDB = async() => {
+    await Campground.deleteMany({});
+    for(let i = 0; i < 50; i++){
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground ({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`
+        })
+        await camp.save();
+    }
+}
+
+seedDB();
